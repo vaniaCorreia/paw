cb1a670d774f745149c98e86132bc02e717cced8
vaniaCorreia <8060037@estg.ipp.pt>
2020-09-06 03:07:53 +0100

    first commit

--- api/server.js
+++ api/server.js
@@ -10,4 +10,6 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
-app.listen(3000);
+//Servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>  console.log('Servidor na porta ' + PORT + '.'));
